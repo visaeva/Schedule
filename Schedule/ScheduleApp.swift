5cdcdecc8201c304cd07eadbeebebe36dc4db7a1
visaeva <visaeva0302@gmail.com>
2024-03-01 18:00:49 +0300

    Initial Commit

---
+++ Schedule/ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleApp.swift
+//  Schedule
+//
+//  Created by Victoria Isaeva on 01.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
